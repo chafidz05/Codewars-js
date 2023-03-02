27f3b6d2d62c412b8e2ed895fc723d1826f0cdc1
chafidz05 <chafidz0505@gmail.com>
2023-03-02 17:59:45 +0700

    parseInt() reloaded

---
+++ 4-kyu/parseInt() reloaded.js
@@ -0,0 +1,14 @@
+var words = {
+  "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9,
+  "ten": 10, "eleven": 11, "twelve": 12, "thirteen": 13, "fourteen": 14, "fifteen": 15, "sixteen": 16, "seventeen": 17,
+  "eighteen": 18, "nineteen": 19, "twenty": 20, "thirty": 30, "forty": 40, "fifty": 50, "sixty": 60, "seventy": 70, "eighty": 80,
+  "ninety": 90
+};
+var mult = { "hundred": 100, "thousand": 1000, "million": 1000000 };
+function parseInt(str) {
+  return str.split(/ |-/).reduce(function(value, word) {
+    if(words[word]) value += words[word];
+    if(mult[word]) value += mult[word] * (value % mult[word]) - (value % mult[word]);
+    return false;
+  }, 0);
+}

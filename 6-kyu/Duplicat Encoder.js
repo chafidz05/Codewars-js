825d4d01f209bb1385ec08dc2c6eaad0de1e5d20
chafidz05 <chafidz0505@gmail.com>
2023-03-02 23:55:21 +0700

    Duplicate Encoder

---
+++ 6-kyu/Duplicat Encoder.js
@@ -0,0 +1,16 @@
+function duplicateEncode(word) {
+  let chars = [...word.toLowerCase()];
+  let duplicateList = chars.filter((char, index, chars) => chars.indexOf(char) !== index);
+  let duplicateSet = new Set(duplicateList);
+  let uniqueDuplicateList = [...duplicateSet];
+  
+  let resultString = "";
+  for(let i = 0, n = chars.length; i < n; ++i) {
+    if(uniqueDuplicateList.includes(chars[i])) {
+      resultString += ")";
+    } else {
+      resultString += "(";
+    }
+  }
+  return resultString;
+}

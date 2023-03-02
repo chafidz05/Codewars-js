078af22d9587e11e7fc25415cc17e2c9c3721211
chafidz05 <chafidz0505@gmail.com>
2023-03-02 23:57:33 +0700

    Find The Parity Outlier

---
+++ 6-kyu/Find The Parity Outlier.js
@@ -0,0 +1,18 @@
+function findOutlier(integers) {
+  let oddArr = [];
+  let evenArr = [];
+
+  for(let i = 0; i < integers.length; i++) {
+    if(integers[i] % 2 === 0) {
+      evenArr.push(integers[i]);
+    } else {
+      oddArr.push(integers[i]);
+    }
+  }
+
+  if(evenArr.length == 1) {
+    return evenArr[0];
+  } else {
+    return oddArr[0];
+  }
+}

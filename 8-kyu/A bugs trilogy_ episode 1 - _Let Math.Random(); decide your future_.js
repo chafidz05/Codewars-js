a446d2f234cbc60b73d694dbdffedb2a140ea0aa
chafidz05 <chafidz0505@gmail.com>
2023-03-28 18:38:22 +0700

    A bugs trilogy: Episode 1 - Let Math.Random(); decide your future

---
+++ 8-kyu/A bugs trilogy_ episode 1 - _Let Math.Random(); decide your future_.js
@@ -0,0 +1,6 @@
+function yourFutureCareer() {
+  var career = Math.random()
+  if(career <= 0.32) return 'FrontEnd Developer';
+  if(career <= 0.65) return 'BackEnd Developer';
+  return 'Full-Stack Developer';
+}

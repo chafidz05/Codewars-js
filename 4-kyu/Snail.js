76052b496521764cc4af3668e8a91099cefea866
chafidz05 <chafidz0505@gmail.com>
2023-04-09 01:31:54 +0700

    Snail

---
+++ 4-kyu/Snail.js
@@ -0,0 +1,9 @@
+function snail(array) {
+  var vector = [];
+  while(array.length) {
+    vector.push(...array.shift());
+    array.map(row => vector.push(row.pop()));
+    array.reverse().map(row => row.reverse());
+  }
+  return vector;
+}

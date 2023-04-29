2cd1ae2e33ac02d9f6310231cf768985b9e15599
chafidz05 <chafidz0505@gmail.com>
2023-04-29 23:55:26 +0700

    Best travel

---
+++ 5-kyu/Best travel.js
@@ -0,0 +1,10 @@
+const chooseBestSum = (t, k, ls) =>
+  ls
+    .reduce(
+      (r, e) => r.concat(r.filter(c => c.length < k).map(c => c.concat([e]))),
+      [[]]
+    )
+    .filter(c => c.length === k)
+    .map(c => c.reduce((a, b) => a + b))
+    .filter(s => s <= t)
+    .sort((a, b) => b - a)[0] || null;
